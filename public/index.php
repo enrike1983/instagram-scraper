b6df854c3483a3054974c412b0543ab4d313b049
Enrico Antonello <eantonel@gmail.com>
2018-09-13 14:42:55 +0200

    frontend

--- public/index.php
+++ public/index.php
@@ -1,18 +1,18 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-if(file_exists(__DIR__.'/.env')) {
+if(file_exists(__DIR__.'/../.env')) {
     //dotenv
-    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv = new Dotenv\Dotenv(__DIR__.'/../');
     $dotenv->load();
 }
 
 ////logger
 $log = new Logger('name');
-$log->pushHandler(new StreamHandler(__DIR__ .'/logs/calls_results.log', Logger::INFO));
+$log->pushHandler(new StreamHandler(__DIR__ .'/../logs/calls_results.log', Logger::INFO));
 //
 ////db
 $username = getenv('USERNAME');
@@ -21,27 +21,12 @@ $dsn = getenv('DSN');
 
 $dbh = new PDO($dsn, $username, $password);
 
-//instagram
-//echo "Id: {$media->getId()}\n";
-//echo "Shotrcode: {$media->getShortCode()}\n";
-//echo "Created at: {$media->getCreatedTime()}\n";
-//echo "Caption: {$media->getCaption()}\n";
-//echo "Number of comments: {$media->getCommentsCount()}";
-//echo "Number of likes: {$media->getLikesCount()}";
-//echo "Get link: {$media->getLink()}";
-//echo "High resolution image: {$media->getImageHighResolutionUrl()}";
-//echo "Media type (video or image): {$media->getType()}";
-//$account = $media->getOwner();
-//echo "Account info:\n";
-//echo "Id: {$account->getId()}\n";
-//echo "Username: {$account->getUsername()}\n";
-//echo "Full name: {$account->getFullName()}\n";
-//echo "Profile pic url: {$account->getProfilePicUrl()}\n";
-
 try {
-    $feeds = $dbh->query('SELECT instagram_id from feeds', PDO::FETCH_ASSOC);
+    $stmt = $dbh->query('SELECT * from feeds');
+    $feeds = $stmt->fetch();
 
     header('Content-Type: application/json');
+
     echo json_encode($feeds);
 
 } catch (\Exception $e) {
